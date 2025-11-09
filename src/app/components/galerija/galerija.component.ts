ffda82a8aca1f263daef1035d6da604aa10a4887
ndobrosavljevic <n.dobrosavljevic01@gmail.com>
2025-11-09 17:11:53 +0100

    add new images in galerija

--- src/app/components/galerija/galerija.component.ts
+++ src/app/components/galerija/galerija.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-galerija',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './galerija.component.html',
   styleUrl: './galerija.component.css'
 })
 export class GalerijaComponent {
+    images = [
+    'cg_2.webp',
+    'cg_3.webp',
+    'cg_nocna.webp',
+    'master.webp',
+    'zemun.webp',
+    'rados.webp'
+  ];
 
+  positions = [
+    'object-[center_top]',   // 1
+    'object-[center_20%]',   // 2
+    'object-[center_25%]',   // 3
+    'object-[center_60%]',         // 4
+    'object-[center_40%]',   // 5
+    'object-[center_75%]',   // 6
+  ];
 }
