5359e3fcbfa99a1ab76f329e33d14c9a4f836538
Nenad Dobrosavljevic <n.dobrosavljevic01@gmail.com>
2026-04-04 22:48:48 +0200

    refactor(home): add better alt

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -21,23 +21,112 @@ export class HomeComponent implements OnInit {
     private seo: SeoService
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.maske = this.maskaService.getMaske();
 
     this.seo.updateSeo({
-      title: 'Maske za klimu | Dekorativne i zaštitne maske za klime',
-      description: 'Maske za klimu od plastificiranog lima za spoljne jedinice klima uređaja. Dekorativne i zaštitne maske za klime, izrada po meri, izbor boja i dostava širom Srbije.',
+      title: 'Maske za klimu | Maska za klimu po meri | Maske za klime cena',
+      description: 'Maske za klimu od plastificiranog lima za spoljne jedinice klima uređaja. Maska za klimu po meri, ukrasne i zaštitne maske za klime, cena standardnog modela 12.480 RSD, dostava širom Srbije.',
       url: 'https://maskezaklimu.rs/',
       image: 'https://maskezaklimu.rs/assets/maska-za-klimu-sitni-listovi.webp'
     });
 
-    this.seo.setJsonLd('home-page-schema', {
+    this.seo.setJsonLd('home-organization-schema', {
       '@context': 'https://schema.org',
-      '@type': 'CollectionPage',
+      '@type': 'Organization',
+      '@id': 'https://maskezaklimu.rs/#organization',
+      name: 'Maske za klimu',
+      alternateName: 'SZMR Đorđević',
+      legalName: 'SZMR Đorđević',
+      url: 'https://maskezaklimu.rs/',
+      logo: 'https://maskezaklimu.rs/assets/maska_za_klimu_logo.png',
+      telephone: '+381659775995',
+      email: 'n.dobrosavljevic01@gmail.com',
+      sameAs: [
+        'https://www.facebook.com/maskezaklimu',
+        'https://www.instagram.com/maske_za_klimu'
+      ]
+    });
+
+    this.seo.setJsonLd('home-localbusiness-schema', {
+      '@context': 'https://schema.org',
+      '@type': 'LocalBusiness',
+      '@id': 'https://maskezaklimu.rs/#localbusiness',
       name: 'Maske za klimu',
       url: 'https://maskezaklimu.rs/',
-      description: 'Dekorativne i zaštitne maske za spoljne jedinice klima uređaja.',
-      inLanguage: 'sr-RS'
+      image: 'https://maskezaklimu.rs/assets/maska-za-klimu-sitni-listovi.webp',
+      telephone: '+381659775995',
+      priceRange: 'RSD 10980-12480',
+      address: {
+        '@type': 'PostalAddress',
+        streetAddress: 'Vučačka 16',
+        addressLocality: 'Smederevo',
+        postalCode: '11300',
+        addressCountry: 'RS'
+      },
+      areaServed: {
+        '@type': 'Country',
+        name: 'Serbia'
+      }
+    });
+
+    this.seo.setJsonLd('home-webpage-schema', {
+      '@context': 'https://schema.org',
+      '@type': 'WebPage',
+      '@id': 'https://maskezaklimu.rs/#webpage',
+      name: 'Maske za klimu | Maska za klimu po meri | Maske za klime cena',
+      url: 'https://maskezaklimu.rs/',
+      description: 'Maske za klimu od plastificiranog lima za spoljne jedinice klima uređaja.',
+      inLanguage: 'sr-RS',
+      primaryImageOfPage: {
+        '@type': 'ImageObject',
+        url: 'https://maskezaklimu.rs/assets/maska-za-klimu-sitni-listovi.webp'
+      }
+    });
+
+    this.seo.setJsonLd('home-itemlist-schema', {
+      '@context': 'https://schema.org',
+      '@type': 'ItemList',
+      name: 'Maske za klimu',
+      itemListOrder: 'https://schema.org/ItemListOrderAscending',
+      numberOfItems: this.maske.length,
+      itemListElement: this.maske.map((maska, index) => ({
+        '@type': 'ListItem',
+        position: index + 1,
+        url: `https://maskezaklimu.rs/proizvod/${maska.slug}`,
+        name: maska.naziv
+      }))
+    });
+
+    this.seo.setJsonLd('home-faq-schema', {
+      '@context': 'https://schema.org',
+      '@type': 'FAQPage',
+      mainEntity: [
+        {
+          '@type': 'Question',
+          name: 'Kolika je cena maske za klimu?',
+          acceptedAnswer: {
+            '@type': 'Answer',
+            text: 'Standardna cena za masku dimenzija 900 × 650 × 440 mm iznosi 12.480 RSD. Za druge dimenzije radimo ponudu po meri.'
+          }
+        },
+        {
+          '@type': 'Question',
+          name: 'Da li maska utiče na rad klime?',
+          acceptedAnswer: {
+            '@type': 'Answer',
+            text: 'Ne. Naše maske za klimu projektovane su tako da ne ometaju protok vazduha i normalan rad uređaja.'
+          }
+        },
+        {
+          '@type': 'Question',
+          name: 'Da li izrađujete masku za klimu po meri?',
+          acceptedAnswer: {
+            '@type': 'Answer',
+            text: 'Da. Možemo izraditi masku za klimu po meri prema dimenzijama vaše spoljne jedinice i željenoj boji.'
+          }
+        }
+      ]
     });
   }
 }
